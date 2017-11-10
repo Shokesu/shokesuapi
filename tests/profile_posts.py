653d9292a42b2efddae60ca3aa1119f4ec775a65
Vykstorm <victorruizgomezdev@gmail.com>
2017-11-10 11:50:17 +0100

    Añadido un nuevo ejemplo (para usar el método get_profile_posts de la API)

---
+++ tests/profile_posts.py
@@ -0,0 +1,36 @@
+
+'''
+Este script es un ejemplo de como usar la API de Shokesu para obtener posts asociados a un
+perfil monotorizado.
+'''
+
+from api import API
+from logger import Logger
+import logging
+from entities.post import Post
+
+if __name__ == '__main__':
+    # La siguiente sentencia permite guardar los mensajes de depuración en un fichero log
+    # externo.
+    Logger().setLevel(logging.DEBUG)
+
+    # Creamos el objeto API. Necesitamos el JWT para tener acceso.
+    api = API(access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiAiMTJmMzNlODJjNWE1NGZiZmE2MmYxZDJkZmQ5YzBkNmIifQ.B1pmP0j4jp33MA4uwBZUkpL6JnRjamTT3RQjgbHiUeA')
+
+    posts = api.get_profile_posts(
+        profile = '46078800', # ID del perfil del cual queremos obtener sus posts
+        page_number = 1,
+        page_size = 5,
+        sort = 'retweets_desc'
+    )
+
+    # Imprimimos los mensajes de los posts.
+    for post in posts:
+        print(post.body)
+
+    # Para obtener un listado con los campos disponibles (para el primer post)...
+    if len(posts) > 0:
+        print(list(posts[0].to_dict().keys()))
+
+    # Para obtener un listado de los campos de la entidad Post (y metainformación de los mismos)...
+    print(Post.get_fields())
